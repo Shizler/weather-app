d31d8019b302d8e9fb5ffab7ed643ecefc903b6e
Shizler <youremail@domain.com>
2021-07-14 12:33:03 +0300

    making the ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import cloudy from './assets/images/forest_cloudy.png'
+import sunny from './assets/icons/partlysunny@2x.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="bg">
+        <img src={cloudy} alt="" />
+        <div className="top">
+          <p style={{fontSize:"50px"}}>18</p>
+          <p style={{fontSize:"22px"}}>CLOUDY</p>
+        </div>
+      </div>
+      <div className="main">
+        <table>
+        <tr>
+            <td>16</td>
+            <td>18</td>
+            <td>23</td>
+          </tr>
+          <tr>
+            <td>min</td>
+            <td>Current</td>
+            <td>max</td>
+          </tr>
+        </table>
+      </div>
+      <div className="table">
+        <table>
+          <tr>
+            <td>Tuesday</td>
+            <td><img src={sunny} alt=""/></td>
+            <td>20</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td><img src={sunny} alt=""/></td>
+            <td>18</td>
+          </tr>
+          <tr>
+            <td>Thurday</td>
+            <td><img src={sunny} alt=""/></td>
+            <td>17</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
